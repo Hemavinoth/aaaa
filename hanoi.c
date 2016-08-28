59f0fec6eea89ba5587ef77d64ddcc676f75ce86
hemalatha <avhemavinoth@gmail.com>
2016-08-28 10:59:42 +0530

    Update hanoi.c

--- hanoi.c
+++ hanoi.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 void hanoi(int n,char from,char topeg,char aux){
-    if(num==1){
-        printf("\nMoving disc 1 from peg %c to peg %c",from,topeg);
+    if(n==1){
+        printf("\n",from,topeg);
         return;
     }
     hanoi(n-1,from,aux,topeg);
-    printf("\nMoving disc %d from peg %c to peg %c",n,from,top);
-    hanoi(num-1,topeg,from,aux);
+    printf("\n",n,from,top);
+    hanoi(n-1,topeg,from,aux);
 }
 
 int main(){
-    int num;
-    scanf("%d",&num);
-    hanoi(num,'A','C','B');
+    int n;
+    scanf("%d",&n);
+    hanoi(n,'A','C','B');
     return 0;
 }
